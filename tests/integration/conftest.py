42030e38bc82fa2fb6e842adf8abee6b919cfeef
Manuel <manuel.cano@wazuh.com>
2023-06-22 11:14:01 +0200

    Applied some requested minor fixes

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -11,7 +11,7 @@ from wazuh_testing.utils import configuration, database, file, services
 from wazuh_testing.constants import platforms
 from wazuh_testing.constants.daemons import WAZUH_MANAGER
 from wazuh_testing.constants.paths import ROOT_PREFIX
-from wazuh_testing.constants.paths.logs import OSSEC_LOG_PATH, ALERTS_JSON_PATH
+from wazuh_testing.constants.paths.logs import WAZUH_LOG_PATH, ALERTS_JSON_PATH
 
 
 ###############################
@@ -118,9 +118,9 @@ def set_wazuh_configuration(test_configuration: dict) -> None:
 def truncate_monitored_files():
     """Truncate all the log files and json alerts files before and after the test execution"""
     if services.get_service() == WAZUH_MANAGER:
-        log_files = [OSSEC_LOG_PATH, ALERTS_JSON_PATH]
+        log_files = [WAZUH_LOG_PATH, ALERTS_JSON_PATH]
     else:
-        log_files = [OSSEC_LOG_PATH]
+        log_files = [WAZUH_LOG_PATH]
 
     for log_file in log_files:
         if os.path.isfile(os.path.join(ROOT_PREFIX, log_file)):

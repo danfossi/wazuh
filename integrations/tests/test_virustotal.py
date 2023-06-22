7a7f5d558bdb7ac05ecdd7d052c5b697854807bc
Gabriel Valenzuela <gabriel.valenzuela@wazuh.com>
2023-06-22 09:10:08 -0300

    Take changes from original branch, manually merge with master due conflicts and create clean branch with changes

--- integrations/tests/test_virustotal.py
+++ integrations/tests/test_virustotal.py
@@ -5,11 +5,20 @@
 
 """Unit tests for virustotal.py integration."""
 
+import json
 import os
 import pytest
+import requests
+from socket import socket, AF_UNIX, SOCK_DGRAM
 import sys
 import virustotal as virustotal
-from unittest.mock import patch
+from unittest.mock import patch, mock_open
+
+# Exit error codes
+ERR_NO_APIKEY           = 1
+ERR_BAD_ARGUMENTS       = 2
+ERR_FILE_NOT_FOUND      = 6
+ERR_INVALID_JSON        = 7
 
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..')) #Necessary to run PyTest
 
@@ -129,3 +138,97 @@ def test_request_virustotal_info_md5_after_check_ok():
     with patch('virustotal.query_api'), patch('virustotal.in_database', return_value=False), patch('virustotal.debug') as debug:
         response = virustotal.request_virustotal_info(alert_template[8],apikey_virustotal)
         assert response == alert_output
+
+def test_main_bad_arguments_exit():
+    """Test that main function exits when wrong number of arguments are passed."""
+    with patch("virustotal.open", mock_open()), pytest.raises(SystemExit) as pytest_wrapped_e:
+        virustotal.main(sys_args_template[0:2])
+    assert pytest_wrapped_e.value.code == ERR_BAD_ARGUMENTS
+
+def test_main_exception():
+    """Test exception handling in main when process_args raises an exception."""
+    with patch("virustotal.open", mock_open()), pytest.raises(Exception),patch('virustotal.process_args') as process:
+        process.side_effect = Exception
+        virustotal.main(sys_args_template)
+
+def test_main():
+    """Test the correct execution of the main function."""
+    with patch("virustotal.open", mock_open()), patch('json.load', return_value=alert_template),\
+        patch('requests.post', return_value=requests.Response), patch('virustotal.process_args') as process:
+        virustotal.main(sys_args_template)
+        process.assert_called_once_with(sys_args_template)
+
+@pytest.mark.parametrize('side_effect, return_value', [
+    (FileNotFoundError, ERR_FILE_NOT_FOUND),
+    (json.decoder.JSONDecodeError("Expecting value", "", 0), ERR_INVALID_JSON)
+])
+def test_process_args_exit(side_effect, return_value):
+    """Test the process_args function exit codes.
+
+    Parameters
+    ----------
+    side_effect : Exception
+        Exception to be raised when there is a failure inside the Load alert section try.
+    return_value : int
+        Value to be returned when sys.exit() is invoked.
+    """
+    with patch("virustotal.open", mock_open()), \
+            patch('json.load') as json_load, \
+            pytest.raises(SystemExit) as pytest_wrapped_e:
+        json_load.side_effect = side_effect
+        virustotal.process_args(sys_args_template)
+    assert pytest_wrapped_e.value.code == return_value
+
+def test_process_args():
+    """Test the correct execution of the process_args function."""
+    with patch("virustotal.open", mock_open()), \
+            patch('virustotal.get_json_alert') as alert_load,\
+            patch('virustotal.send_msg') as send_msg, \
+            patch('virustotal.generate_msg', return_value=alert_output) as generate_msg, \
+            patch('requests.post', return_value=requests.Response):
+        alert_load.return_value = alert_template
+        virustotal.process_args(sys_args_template)
+        generate_msg.assert_called_once_with(alert_template,sys_args_template[2])
+        generated_msg = virustotal.generate_msg(alert_template,sys_args_template[2])
+        assert generated_msg==alert_output
+        send_msg.assert_called_once_with(alert_output,alert_output['agent'])
+
+def test_process_args_not_sending_message():
+    """Test that the send_msg function is not executed due to empty message after generate_msg."""
+    with patch("virustotal.open", mock_open()), \
+            patch('virustotal.get_json_alert') as alert_load,\
+            patch('virustotal.send_msg') as send_msg, \
+            patch('virustotal.generate_msg', return_value=''), \
+            pytest.raises(Exception):
+        alert_load.return_value = alert_template
+        virustotal.process_args(sys_args_template)
+        send_msg.assert_not_called()
+
+def test_debug():
+    """Test the correct execution of the debug function, writing the expected log when debug mode enabled."""
+    with patch('virustotal.debug_enabled', return_value=True), \
+            patch("virustotal.open", mock_open()) as open_mock, \
+            patch('virustotal.LOG_FILE', return_value='integrations.log') as log_file:
+        virustotal.debug(alert_output)
+        open_mock.assert_called_with(log_file, 'a')
+        open_mock().write.assert_called_with(alert_output)
+
+
+def test_send_msg_raise_exception():
+    """Test that the send_msg function will raise an exception when passed the wrong webhook url."""
+    with patch('requests.post'), \
+        pytest.raises(SystemExit):
+        virustotal.send_msg(alert_output,alert_output['agent'])
+
+
+def test_send_msg():
+    """Test that the send_msg function works as expected."""
+    headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
+    with patch('virustotal.SOCKET_ADDR',"./socket.sock"):
+        with socket(AF_UNIX, SOCK_DGRAM) as s:
+            s.bind("./socket.sock")
+            virustotal.send_msg(alert_output, sys_args_template[3])
+            data = s.recvfrom(1024)
+            assert data[0].decode() != None
+            s.close()
+        os.remove('./socket.sock')
